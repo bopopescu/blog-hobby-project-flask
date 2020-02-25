a6c67905db11c72d76f95fded9ae33d79f8d028e
Akash Chhetri <akash.chhetri1@gmail.com>
2020-02-25 12:33:15 +1100

    04- Added MySQL local database connection in contact form

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from datetime import datetime
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:akash12345@localhost/codingblog'
+db = SQLAlchemy(app)
+
+
+# define class for database ORM
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    phone_num = db.Column(db.String(12))
+    msg = db.Column(db.String(120))
+    date = db.Column(db.String(120))
+    email = db.Column(db.String(20))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
 
 
 @app.route('/')
@@ -11,12 +34,28 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods = ['GET','POST'])
 def contact():
+    # add value to db
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name,phone_num=phone,msg=message,date=datetime.now(),email=email)
+        db.session.add(entry)
+        db.session.commit()
+        print("Phase 5")
+        print("\n\n")
     return render_template('contact.html')
 
 @app.route('/post')
 def post():
     return render_template('post.html')
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run()    
+    db.create_all()
+
+
+
